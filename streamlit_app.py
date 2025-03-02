63017d7c3e2c0f5fd84e5a8f4602ef320876fc8b
Hemanth <57652459+vhshemanth@users.noreply.github.com>
2025-03-02 16:59:40 +0530

    Update streamlit_app.py
    Space fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ if ingredients_list:
         
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-        st.subheader(fruit_chosen + 'Nutrional Information')
+        st.subheader(fruit_chosen + ' Nutrional Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         
         #st.text(smoothiefroot_response.json())
